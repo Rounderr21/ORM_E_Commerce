11a57130ebcafcc92e3f5c43e50a5ba94bf161ac
Cody Schwengler01~ <cschwengler@outlook.com>
2023-10-29 16:49:27 -0700

    created category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,25 +3,74 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
+router.get('/', async (req, res) => {
+  try {
+  const categories = await Category.findAll({
+    include: Product
+  });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server Error' });
+  }
+ //find all categories
+
+
+
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      include: Product
+    });
+    res.json(category);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server Error' });
+  }
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try{
+    const category = await Category.create(req.body);
+    res.json(category);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server Error' });
+  }
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+  try{
+    const category = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    res.json(category);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server Error' });
+  }
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+  try{
+    const category = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    res.json(category);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server Error' });
+  }
   // delete a category by its `id` value
 });
 
