a791d66ed5de392e030685b108fd2798992d9409
Cody Schwengler01~ <cschwengler@outlook.com>
2023-10-29 16:19:12 -0700

    created tables within the database

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,32 @@
--- DROP DATABASE
+-- DROP DATABASE IF IT EXISTS
 DROP DATABASE IF EXISTS ecommerce_db;
-
--- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+USE ecommerce_db;
+
+CREATE TABLE category (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    category_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE product (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    product_name VARCHAR(255) NOT NULL,
+    price DECIMAL(10,2) NOT NULL CHECK (price >= 0),
+    stock INT NOT NULL DEFAULT 10 CHECK (stock >= 0 AND stock = CAST(stock AS UNSIGNED)),
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES category(id)
+);
+
+CREATE TABLE tag (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    tag_name VARCHAR(255)
+);
+
+CREATE TABLE productTag (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    product_id INT,
+    tag_id INT,
+    FOREIGN KEY (product_id) REFERENCES product(id),
+    FOREIGN KEY (tag_id) REFERENCES tag(id)
+);
