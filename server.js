fc99e6b8d420d9d87427cc9df04c6b6bcce0c0c1
Cody Schwengler01~ <cschwengler@outlook.com>
2023-10-29 12:37:17 -0700

    finished server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
-const mysql2 = require('mysql2');
-// import sequelize connection
+const sequelize = require('./config/connection');
+// import sequelize connection ^^^
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,9 +9,12 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// turn on routes
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// syncs sequelize models to the database, then turn on the server
+sequelize.sync({force: false}).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
